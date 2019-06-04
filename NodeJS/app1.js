a8514bb9087180e536fece8ea71df67180b85f23
JohnR46 <johnnote7@gmail.com>
2019-06-04 17:57:31 +0700

    add path 5 mkdir

--- NodeJS/app1.js
+++ NodeJS/app1.js
@@ -1,4 +1,10 @@
 const fs = require('fs');
 
-let readMe  = fs.readFileSync("code.txt","utf8");
-console.log(readMe);
+let readMe = fs.readFileSync("code.txt", "utf8");
+
+fs.mkdir('Project', () => { // สร้างโฟล์เดอร์เสร็จละทำไรต่อ 
+    console.log(readMe);
+    fs.writeFileSync('./Project/Readme.txt', readMe);
+
+});
+
